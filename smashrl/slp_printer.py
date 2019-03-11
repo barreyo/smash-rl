129d484e59f77461ce65077642d5c0bbab8a2c39
Johan Backman <johan@cultivateai.com>
2019-03-10 17:45:19 -0700

    Print player positions from replay

--- smashrl/slp_printer.py
+++ smashrl/slp_printer.py
@@ -1,9 +1,14 @@
+"""Prints a .slp file frame by frame."""
 
 from slippi import Game
 
 
 def _main():
-    pass
+    game = Game('Game_20190224T233757.slp')
+
+    for frame in game.frames:
+        data = frame.ports[0].leader  # see also: port.follower (ICs)
+        print(f'Player 1 pos: {frame.ports[0].position}    Player 2 pos: {frame.ports[1].position})
 
 
 if __name__ == '__main__':
