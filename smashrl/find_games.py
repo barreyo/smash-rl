409591570b848bed9ac3410243c7849f0de73219
Kostas Vaggelakos <kostas@vaggelakos.com>
2019-03-17 18:17:04 -0700

    Update find_games to run in multiprocess mode

--- smashrl/find_games.py
+++ smashrl/find_games.py
@@ -1,36 +1,34 @@
 """Prints a .slp file frame by frame."""
 
+from multiprocessing import Pool
 from slippi import Game
 from slippi.id import Stage, CSSCharacter
 import os
 
+
+STAGES = [Stage.BATTLEFIELD, Stage.FINAL_DESTINATION]
+CHARACTERS = [CSSCharacter.FOX]
+
+def is_valid(f):
+    try:
+        print(f"Scanning {f}")
+        game = Game(os.path.join('./data', f))
+        all_characters = [p.character for p in game.start.players if p is not None]
+
+        if game.start.stage in STAGES and len(set(all_characters) & set(CHARACTERS)) > 0:
+            return True
+        return False
+    except Exception:
+        return False
+
 def _main():
+    p = Pool(8)
+    files = [ x for x in os.listdir('./data/') if x.endswith('.slp') ]
+    results = p.map(is_valid, files)
+    p.terminate()
+    p.join()
 
-    STAGES = [Stage.BATTLEFIELD, Stage.FINAL_DESTINATION]
-    CHARACTERS = [CSSCharacter.FOX]
-
-    valid_replays = []
-    invalid_replays = []
-
-
-    for f in os.listdir('./data/'):
-        if not f.endswith('.slp'):
-            continue
-        try:
-            game = Game(os.path.join('./data', f))
-            all_characters = [p.character for p in game.start.players if p is not None]
-
-            if game.start.stage in STAGES and len(set(all_characters) & set(CHARACTERS)) > 0:
-                valid_replays.append(f)
-            else:
-                invalid_replays.append(f)
-        except Exception:
-            invalid_replays.append(f)
-            print("Could not parse: {}".format(f))
-
-    print(f"Found {len(valid_replays)} valid replays")
-    for v in valid_replays:
-        print(v)
+    import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
     _main()
