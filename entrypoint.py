25a1023440aa76a0841108546384c717edd8e9b3
Johan Backman <johan@cultivateai.com>
2019-12-15 16:31:06 -0800

    Add entrypoint logging

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,21 @@
 
+import logging
+
 from smashrl.train import _main as run_training
 from tools.scraper.scrape import _main as download_training_data
 
 
+log = logging.getLogger(__name__)
+
+
 def _main():
+    log.info("Downloading training data...")
     download_training_data('./data')
+
+    log.info("Running training...")
     run_training('./data')
 
 
 if __name__ == "__main__":
+    log.info("Starting")
     _main()
